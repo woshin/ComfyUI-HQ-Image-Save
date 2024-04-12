5ffdc021790fcfbdf90a4a62a3d9d93128e28c04
woshin <woshin@outlook.com>
2024-04-12 10:08:51 +0800

    read exr with openexr

--- nodes.py
+++ nodes.py
@@ -14,6 +14,10 @@ from comfy.cli_args import args
 from comfy.utils import PROGRESS_BAR_ENABLED, ProgressBar
 
 
+import OpenEXR
+import Imath
+
+
 def sRGBtoLinear(npArray):
     less = npArray <= 0.0404482362771082
     npArray[less] = npArray[less] / 12.92
@@ -25,18 +29,68 @@ def linearToSRGB(npArray):
     npArray[~less] = np.power(npArray[~less], 1/2.4) * 1.055 - 0.055
 
 def load_EXR(filepath, sRGB):
-    image = cv.imread(filepath, cv.IMREAD_UNCHANGED).astype(np.float32)
-    rgb = np.flip(image[:,:,:3], 2).copy()
+    exr_file = OpenEXR.InputFile(filepath)
+    header = exr_file.header()
+    dw = header['dataWindow']
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+    pt = Imath.PixelType(Imath.PixelType.FLOAT)
+
+    # 读取RGB通道
+    r_str = exr_file.channel('ViewLayer.Combined.R', pt)
+    g_str = exr_file.channel('ViewLayer.Combined.G', pt)
+    b_str = exr_file.channel('ViewLayer.Combined.B', pt)
+    a_str = exr_file.channel('ViewLayer.Combined.A', pt)
+
+    d_str = exr_file.channel('ViewLayer.Depth.Z', pt)
+
+    nx_str = exr_file.channel('ViewLayer.Normal.X', pt)
+    ny_str = exr_file.channel('ViewLayer.Normal.Y', pt)
+    nz_str = exr_file.channel('ViewLayer.Normal.Z', pt)
+
+    i_str = exr_file.channel('ViewLayer.IndexOB.X', pt)
+
+    # 将字符串转换为numpy数组
+    r = np.fromstring(r_str, dtype=np.float32)
+    g = np.fromstring(g_str, dtype=np.float32)
+    b = np.fromstring(b_str, dtype=np.float32)
+    a = np.fromstring(a_str, dtype=np.float32)
+
+    d = np.fromstring(d_str, dtype=np.float32)
+    nx = np.fromstring(nx_str, dtype=np.float32)
+    ny = np.fromstring(ny_str, dtype=np.float32)
+    nz = np.fromstring(nz_str, dtype=np.float32)
+
+    i = np.fromstring(i_str, dtype=np.float32)
+
+    # 重新整理数组至正确的图像维度
+    r.shape = g.shape = b.shape = a.shape = d.shape = nx.shape = ny.shape = nz.shape = i.shape = (size[1], size[0])
+
+    d -= np.min(d)
+    d /= np.max(d[i > 0])
+    d = 1.0 - d
+    d[i < 1] = 0
+
+    # 合并三个通道至一个三维数组
+    rgb = np.stack((r, g, b), axis=-1)
+    depth = np.stack((d, d, d), axis=-1)
+    normal = np.stack((nx, ny, nz), axis=-1)
+
+    # 归一化到0-1之间
+    rgb_normalized = np.clip(rgb, 0, 1)
+    depth_normalized = np.clip(depth, 0, 1)
+    normal_normalized = (normal + 1.0) / 2.0 #np.clip(normal, 0, 1)
+
     if sRGB:
-        linearToSRGB(rgb)
-        rgb = np.clip(rgb, 0, 1)
-    rgb = torch.unsqueeze(torch.from_numpy(rgb), 0)
+        linearToSRGB(rgb_normalized)
+        rgb_normalized = np.clip(rgb_normalized, 0, 1)
+    rgb = torch.unsqueeze(torch.from_numpy(rgb_normalized), 0)
+    depth = torch.unsqueeze(torch.from_numpy(depth_normalized), 0)
+    normal = torch.unsqueeze(torch.from_numpy(normal_normalized), 0)
+
+    mask = torch.zeros((1, size[1], size[0]), dtype=torch.float32)
+    mask[0] = torch.from_numpy(i)
     
-    mask = torch.zeros((1, image.shape[0], image.shape[1]), dtype=torch.float32)
-    if image.shape[2] > 3:
-        mask[0] = torch.from_numpy(np.clip(image[:,:,3], 0, 1))
-    
-    return (rgb, mask)
+    return (rgb, normal, depth, mask)
 
 def load_EXR_latent(filepath):
     image = cv.imread(filepath, cv.IMREAD_UNCHANGED).astype(np.float32)
@@ -62,8 +116,8 @@ class LoadEXR:
 
     CATEGORY = "image"
 
-    RETURN_TYPES = ("IMAGE", "MASK", "INT")
-    RETURN_NAMES = ("RGB", "alpha", "batch_size")
+    RETURN_TYPES = ("IMAGE", "IMAGE", "IMAGE", "MASK", "INT")
+    RETURN_NAMES = ("RGB", "normal", "depth", "alpha", "batch_size")
     FUNCTION = "load"
 
     def load(self, filepath, linear_to_sRGB=True, image_load_cap=0, skip_first_images=0, select_every_nth=1):
@@ -72,10 +126,12 @@ class LoadEXR:
             raise Exception("Path not found: " + p)
         
         if os.path.isfile(p) and os.path.splitext(p)[1].lower() == ".exr":
-            rgb, mask = load_EXR(p, linear_to_sRGB)
+            rgb, normal, depth, mask = load_EXR(p, linear_to_sRGB)
             batch_size = 1
         else:
             rgb = []
+            normal = []
+            depth = []
             mask = []
             filelist = sorted(glob.glob(os.path.join(p, "*.exr")))
             if not filelist:
@@ -92,16 +148,20 @@ class LoadEXR:
             if PROGRESS_BAR_ENABLED:
                 pbar = ProgressBar(batch_size)
             for file in tqdm(filelist, desc="loading images"):
-                rgbFrame, maskFrame = load_EXR(file, linear_to_sRGB)
+                rgbFrame, normalFrame, depthFrame, maskFrame = load_EXR(file, linear_to_sRGB)
                 rgb.append(rgbFrame)
+                normal.append(normalFrame)
+                depth.append(depthFrame)
                 mask.append(maskFrame)
                 if PROGRESS_BAR_ENABLED:
                     pbar.update(1)
             
             rgb = torch.cat(rgb, 0)
+            normal = torch.cat(normal, 0)
+            depth = torch.cat(depth, 0)
             mask = torch.cat(mask, 0)
         
-        return (rgb, mask, batch_size)
+        return (rgb, normal, depth, mask, batch_size)
 
 class SaveEXR:
     def __init__(self):
@@ -177,7 +237,19 @@ class SaveEXR:
             
             if os.path.exists(writepath):
                 raise Exception("File exists already, stopping to avoid overwriting")
-            cv.imwrite(writepath, bgr[i])
+                
+            header = OpenEXR.Header(bgr[i].shape[0], bgr[i].shape[1])
+            half_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
+            header['channels'] = dict([(c, half_chan) for c in "RGB"])
+
+            red_str = bgr[i][:,:,2].tobytes()
+            green_str = bgr[i][:,:,1].tobytes()
+            blue_str = bgr[i][:,:,0].tobytes()
+
+            exr_file = OpenEXR.OutputFile(writepath, header)
+            exr_file.writePixels({'R': red_str, 'G': green_str, 'B': blue_str})
+            exr_file.close()
+
             if pbar is not None:
                 pbar.update(1)
 
